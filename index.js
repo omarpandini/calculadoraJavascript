23753244affdc59c00ecaa0d7a35ecf7733d3a78
Omar Pandini <omarp@joinville.unimedsc.com.br>
2022-08-12 15:25:20 -0300

    melhorias

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 ﻿console.log('version 1.00');
 
-//define os botoes
-var btn1 = document.getElementById('btn1');
-var btn2 = document.getElementById('btn2');
-var btn3 = document.getElementById('btn3');
-var btn4 = document.getElementById('btn4');
-var btn5 = document.getElementById('btn5');
-var btn6 = document.getElementById('btn6');
-var btn7 = document.getElementById('btn7');
-var btn8 = document.getElementById('btn8');
-var btn9 = document.getElementById('btn9');
+//Percorre os botões 0 a 9, e cria as funções
+var inputs = document.getElementsByClassName('divNumbers');
+for (let index = 0; index < inputs.length; index++) {       
+    inputs[index].childNodes[1].onclick = fncBtn;    
+}
+
 
 var btnSum = document.getElementById('btnSum');
 var btnEqual = document.getElementById('btnEqual');
@@ -27,8 +23,6 @@ var sum = [['+',0]];
 //Última operação antes do igual
 var lastOperator = '+';
 
-//Ação dos botões
-btn1.onclick = fncBtn1;
 
 btnSum.onclick = function (){
     fncBtSum('+');
@@ -47,7 +41,7 @@ btnClear.onclick = fncBtnClear;
 
 
 //Criação das funções
-function fncBtn1() {
+function fncBtn() {
     inputVal.value += this.value;
 }
 
